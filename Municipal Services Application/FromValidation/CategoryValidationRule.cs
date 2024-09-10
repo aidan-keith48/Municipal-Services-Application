2136bd084b52fc16f7e112715e457f0f6a437502
Aidan Naidoo <ST10109482@vcconnect.edu.za>
2024-09-10 22:08:30 +0200

    Fixed Design and ironed out bugs

---
+++ Municipal Services Application/FromValidation/CategoryValidationRule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Municipal_Services_Application.FromValidation
+{
+    public class CategoryValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if (value is string selectedItem && !string.IsNullOrEmpty(selectedItem))
+            {
+                return ValidationResult.ValidResult;
+            }
+            return new ValidationResult(false, "Selection is required.");
+        }
+    }
+}
--- Municipal Services Application/Views/ReportIssuesView.xaml.cs
+++ Municipal Services Application/Views/ReportIssuesView.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -18,11 +19,27 @@ namespace Municipal_Services_Application.Views
     /// <summary>
     /// Interaction logic for ReportIssuesView.xaml
     /// </summary>
-    public partial class ReportIssuesView : UserControl
+    public partial class ReportIssuesView : System.Windows.Controls.UserControl
     {
         public ReportIssuesView()
         {
             InitializeComponent();
+        }
+
+        private void AttachFileBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Multiselect = true;
+            openFileDialog.Filter = "Image files (*.png;*.jpeg)|*.png;*.jpeg|All files (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Handle file selections, possibly setting some UI element to show selected file names
+            }
+        }
+
+        private void SubmitReportBtn_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.MessageBox.Show("Report submitted successfully!");
         }
     }
 }
